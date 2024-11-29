648495327c7af17c1e4d095395f6f2d7a80bfdb0
J.P Lee <koreanfeel@gmail.com>
2024-11-29 19:07:48 +0900

    불필요 파일 삭제

--- example.py
+++
@@ -1,9 +0,0 @@
-from config import *
-
-from langchain_openai import ChatOpenAI
-
-model = ChatOpenAI(model=GPT_MODEL, temperature=0.0)
-
-result = model.invoke("What is 2 🦜 9?")
-
-print(result.content)
